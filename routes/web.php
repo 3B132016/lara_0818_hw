3fff269fc3767d5ed92c31edf71b2f9a983ba1de
3B132016 <tsengjay0gmail.com>
2024-12-31 05:10:28 +0800

    測試網頁有問題 更改web.php

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
-Route::get('/about', [AboutController::class, 'index'])->name('about.index');
-Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::get('/news', [NewsController::class, 'index'])->name('news');
